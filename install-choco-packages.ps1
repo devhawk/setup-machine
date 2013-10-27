42bd6b21d0b32860ee0e0a9135a37ddc7dffc178
Harry Pierson <harry@devhawk.net>
2013-10-27 11:01:49 -0700

    Added command to copy notepad2.exe to windows direcotry + setup 'Open with Notepad2' shell integration

--- install-choco-packages.ps1
+++ install-choco-packages.ps1
@@ -1,5 +1,10 @@
 cinst git.install 
+cinst gitextensions
 cinst 7zip.install 
 cinst nodejs.install 
 cinst notepad2 
-cinst gitextensions
+
+#add shell command for Notepad2
+copy-item "C:\Program Files\Notepad2\Notepad2.exe" C:\Windows\n2.exe
+reg add HKCR\*\shell\Notepad2 /d "Open with Notepad2"
+reg add HKCR\*\shell\Notepad2\command /d 'n2.exe \"%1\"'
