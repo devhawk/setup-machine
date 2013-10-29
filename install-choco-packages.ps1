aa31872cdfad2295be3464db6546f093a999a58e
Harry Pierson <harry@devhawk.net>
2013-10-29 14:12:37 -0700

    add script to configure my standard git user and alias settings

--- install-choco-packages.ps1
+++ install-choco-packages.ps1
@@ -8,3 +8,11 @@ cinst notepad2
 copy-item "C:\Program Files\Notepad2\Notepad2.exe" C:\Windows\n2.exe
 reg add HKCR\*\shell\Notepad2 /d "Open with Notepad2"
 reg add HKCR\*\shell\Notepad2\command /d 'n2.exe \"%1\"'
+
+#add git config settings
+git config --global user.name "Harry Pierson"
+git config --global user.email "harry@devhawk.net"
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.co checkout
+git config --global alias.ci commit
